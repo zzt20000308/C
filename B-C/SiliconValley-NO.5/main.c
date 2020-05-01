74a9a156f496b38bdd95b54800d17259116a500e
zzt <1975552131@qq.com>
2020-05-01 18:31:31 +0800

    C

--- B-C/SiliconValley-NO.5/main.c
+++ B-C/SiliconValley-NO.5/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+采用先死后活的方式进行编程和思考，先假定number为某个具体的数，然后再把这个数换成变量
 int main()
 {
    int i;
@@ -9,8 +11,8 @@ int main()
        printf("%d+%d=6\n",i,6-i);
    }
 }
+*/
 
-/*
 int main()
 {
     int i,number;
@@ -22,4 +24,4 @@ int main()
     }
     return 0;
 }
-*/
+
